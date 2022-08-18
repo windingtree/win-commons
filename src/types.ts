c1329269af3ed3e85bbc16fd53db8f7bc33a7daf
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-08-18 12:37:16 +0200

    feat: 🎸 Types for the `config` module

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,39 @@ export interface SignatureDomain {
   version: string;
   chainId: number;
 }
+
+// currency the asset is pegged to
+export const assetsCurrencies = ['EUR', 'USD', 'JPY'];
+
+export type AssetCurrency = typeof assetsCurrencies[number];
+
+export interface CryptoAsset {
+  name: string;
+  symbol: string;
+  address: string;
+  decimals: number;
+  image: string;
+  native: boolean;
+  permit: boolean;
+  currency: AssetCurrency;
+}
+
+export interface Network {
+  name: string;
+  chainId: number;
+}
+
+export type NetworkMode = 'dev' | 'stage' | 'prod';
+
+export interface NetworkInfo extends Network {
+  currency: string;
+  decimals: number;
+  rpc: string;
+  contracts: {
+    ledger: string;
+    winPay: string;
+    assets: CryptoAsset[];
+  };
+  blockExplorer: string;
+  mode: NetworkMode[];
+}
