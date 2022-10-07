59e8393bfd6c5ccb921158f71bc457fc1becc7cb
blecorre <104366254+blecorre@users.noreply.github.com>
2022-10-07 10:45:44 +0200

    fix: change the decimals of USDC on Polygon

--- src/config.ts
+++ src/config.ts
@@ -257,7 +257,7 @@ export const allowedNetworks: readonly NetworkInfo[] = Object.freeze([
           symbol: 'USDC',
           address: '0x73B7D1A3799c8C2a73a15A1a4c079B42E817054a',
           coin: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
-          decimals: 18,
+          decimals: 6,
           image: 'https://bafybeihn6h3j36k44zhvxretauc6dy6w6eapjenzmita7pvwrelg4pksvi.ipfs.w3s.link/usdc.png',
           native: false,
           permit: true,
